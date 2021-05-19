dd8c06aeb129e151ddc97ee0e3f6617541cb7f47
kalwalt <info@kalwaltart.it>
2021-05-19 15:25:47 +0200

    types emitted only on types folder

--- dist/utils/Utils.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Utils {
-    constructor();
-    private static trackedMatrix;
-    static interpolate(world: any): any;
-    static isMobile(): boolean;
-    static setMatrix(matrix: any, value: any): void;
-}
