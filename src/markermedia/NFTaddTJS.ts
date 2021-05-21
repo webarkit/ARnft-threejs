424751447a0a1d712b94e306b18d8bda39127ece
kalwalt <github@kalwaltart.it>
2021-05-21 22:09:11 +0200

    new addImage, addVideo, addModel from ARnft

--- src/markermedia/NFTaddTJS.ts
+++ src/markermedia/NFTaddTJS.ts
@@ -1,17 +1,69 @@
-import { Object3D } from 'three'
+import { Object3D,
+         PlaneGeometry,
+         TextureLoader,
+         VideoTexture,
+         Mesh,
+         MeshStandardMaterial } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+interface ARvideo {
+  play: () => void;
+}
 
 export class NFTaddTJS {
-    private modelRoot: Object3D;
+    private root: Object3D;
     constructor(root: Object3D) {
-        this.modelRoot = root;
+        this.root = root;
     }
-
-    public addModel(model: Object3D) {
+    public add(mesh: Object3D) {
         document.addEventListener('getNFTData', (ev: any) => {
             var msg = ev.detail
-            model.position.y = (msg.height / msg.dpi * 2.54 * 10) / 2.0
-            model.position.x = (msg.width / msg.dpi * 2.54 * 10) / 2.0
+            mesh.position.y = (msg.height / msg.dpi * 2.54 * 10) / 2.0
+            mesh.position.x = (msg.width / msg.dpi * 2.54 * 10) / 2.0
         })
-        this.modelRoot.add(model);
+        this.root.add(mesh);
+    }
+    public addModel (url: string, x: number, y: number, z: number, scale: number) {
+        let model
+        /* Load Model */
+        const threeGLTFLoader = new GLTFLoader()
+        threeGLTFLoader.load(url, gltf => {
+            model = gltf.scene
+            model.scale.set(scale, scale, scale)
+            model.rotation.x = Math.PI / 2
+            model.position.x = x
+            model.position.y = y
+            model.position.z = z
+            this.root.add(model)
+        })
+    }
+    public addImage (imageUrl: string, color: string, scale: number) {
+       const planeGeom = new PlaneGeometry(1, 1, 1, 1)
+       const texture = new TextureLoader().load(imageUrl)
+       const material = new MeshStandardMaterial({ color: color, map: texture});
+       const plane = new Mesh(planeGeom, material)
+       plane.scale.set(scale, scale, scale)
+       document.addEventListener('getNFTData', (ev: any) => {
+            var msg = ev.detail
+            plane.position.y = (msg.height / msg.dpi * 2.54 * 10) / 2.0
+            plane.position.x = (msg.width / msg.dpi * 2.54 * 10) / 2.0
+       })
+       this.root.add(plane)
+    }
+    public addVideo (id: string, scale: number) {
+       const root = this.root
+       const ARVideo: HTMLVideoElement = document.getElementById(id) as HTMLVideoElement;
+       const texture = new VideoTexture(ARVideo as HTMLVideoElement)
+       const mat = new MeshStandardMaterial({ color: 0xbbbbff, map: texture })
+       ARVideo.play()
+       const planeGeom = new PlaneGeometry(1, 1, 1, 1)
+       const plane = new Mesh(planeGeom, mat)
+       plane.scale.set(scale, scale, scale)
+       document.addEventListener('getNFTData', (ev: any) => {
+           var msg = ev.detail
+           plane.position.y = (msg.height / msg.dpi * 2.54 * 10) / 2.0
+           plane.position.x = (msg.width / msg.dpi * 2.54 * 10) / 2.0
+       })
+       this.root.add(plane)
     }
 }
--- types/markermedia/NFTaddTJS.d.ts
+++ types/markermedia/NFTaddTJS.d.ts
@@ -1,6 +1,9 @@
 import { Object3D } from 'three';
 export declare class NFTaddTJS {
-    private modelRoot;
+    private root;
     constructor(root: Object3D);
-    addModel(model: Object3D): void;
+    add(mesh: Object3D): void;
+    addModel(url: string, x: number, y: number, z: number, scale: number): void;
+    addImage(imageUrl: string, color: string, scale: number): void;
+    addVideo(id: string, scale: number): void;
 }
