0c32fdff8d4dbc466b13f212cbe71e8838198cff
kalwalt <info@kalwaltart.it>
2021-05-24 00:00:40 +0200

    ARnftThreejs as default export

--- src/ARnftThreejs.ts
+++ src/ARnftThreejs.ts
@@ -1,4 +1,4 @@
 import SceneRendererTJS from "./SceneRendererTJS"
 import NFTaddTJS from "./markermedia/NFTaddTJS"
 
-export { SceneRendererTJS, NFTaddTJS }
+export default { SceneRendererTJS, NFTaddTJS }
