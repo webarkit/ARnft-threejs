c53badc1c650f60775180b31c0703c8d6ffff0f9
Walter Perdan <github@kalwaltart.it>
2024-10-30 20:30:12 +0100

    upgrading three.js to v169 and module version  of the lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,12 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'ARnftThreejs.js',
-    library: 'ARnftThreejs',
-    libraryTarget: 'umd',
-    // @see: https://github.com/webpack/webpack/issues/3929
-    libraryExport: 'default',
-    // @see: https://github.com/webpack/webpack/issues/6522
-    globalObject: 'this',
-  },
-  externals: {
-    three: {
-      commonjs: 'three',
-      commonjs2: 'three',
-      amd: 'three',
-      root: 'THREE' // indicates global variable
-    }
-  },
-  module: {
+module.exports = (env, argv) => {
+  let devtool = false;
+  if (argv.mode === 'development') {
+    devtool = 'inline-source-map';
+  }
+  console.log(`${argv.mode} build`);
+  const module = {
     rules: [
       {
         test: /\.(j|t)sx?$/,
@@ -42,8 +27,60 @@ module.exports = {
       ]
       },
     ],
+  };
+  return [{
+  name: 'ARnftThreejs',
+  entry: './src/index.ts',
+  devtool,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'ARnftThreejs.js',
+    library: 'ARnftThreejs',
+    libraryTarget: 'umd',
+    // @see: https://github.com/webpack/webpack/issues/3929
+    libraryExport: 'default',
+    // @see: https://github.com/webpack/webpack/issues/6522
+    globalObject: 'this',
   },
+  externals: {
+    three: {
+      commonjs: 'three',
+      commonjs2: 'three',
+      amd: 'three',
+      root: 'THREE' // indicates global variable
+    }
+  },
+  module,
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+  }
+},
+{
+  name: 'ARnft-threej.module',
+  entry: './src/index.ts',
+  experiments: {
+    outputModule: true,
   },
+  output: {
+    library: {
+      type: 'module',
+    },
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'ARnftThreejs.module.js',
+    //library: 'ARnftThreejs',
+    //libraryTarget: 'umd',
+    // @see: https://github.com/webpack/webpack/issues/3929
+    //libraryExport: 'default',
+    // @see: https://github.com/webpack/webpack/issues/6522
+    //globalObject: 'this',
+  },
+  externalsType: 'module',
+  externals: {
+    three: 'three',
+  },
+  module,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  }
+}];
 };
