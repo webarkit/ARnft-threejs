2c106402dcfafb5eeac99a8c2b036834d3938537
Walter Perdan <github@kalwaltart.it>
2026-05-05 15:05:16 +0200

    bump version 0.5.4 - upgrading packages and solve issue with tsconfig

--- types/filters/DelayableSignalFilter.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class DelayableSignalFilter {
-    private _inDelay;
-    private _totalTime;
-    private _prevTime;
-    private _timeOut;
-    constructor(timeOut: number);
-    Update(tick: boolean): boolean;
-}
